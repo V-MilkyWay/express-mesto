c0f391e5b0f39bf6bcc1bf3dbf1658a799883f83
Vladislav Milushkov <radshura@yandex.ru>
2021-09-20 21:06:02 +0300

    14+/13

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,93 +1,84 @@
-const Card = require('../models/card.js');
+const Card = require('../models/card');
 
 module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user })
-    .then(card => res.send({ data: card }))
-    .catch(err => {
-      if (err.name === "ValidationError") {
+    .then((card) => res.send({ data: card }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
         // Логика обработки ошибки
-        const err = new Error('Переданы некорректные данные при создании карточки');
-        err.statusCode = 400;
+        const errNew = new Error('Переданы некорректные данные при создании карточки');
+        errNew.statusCode = 400;
 
-        next(err);
+        next(errNew);
       }
       next(err);
     });
 };
 
-
 module.exports.findAllCards = (req, res, next) => {
   Card.find({})
-    .then(card => res.send({ data: card }))
+    .then((card) => res.send({ data: card }))
     .catch(next);
 };
 
 module.exports.findByIdAndRemoveCard = (req, res, next) => {
-
   Card.findById(req.params.cardId)
-    .then(card => {
+    .then((card) => {
       if (!card) {
-
         const err = new Error('Карточка с указанным _id не найден');
         err.statusCode = 404;
 
         next(err);
+      } else if (req.user._id !== card.owner) {
+        const errNew = new Error('Отказано в доступе');
+        errNew.statusCode = 403;
 
+        next(errNew);
       } else {
-        if (req.user._id != card.owner) {
-          const err = new Error('Отказано в доступе');
-          err.statusCode = 403;
-
-          next(err);
-        }
-        else {
-          card.remove()
-            .then((card) => res.send({ data: card }))
-        }
+        card.remove().then(() => res.send({ data: card }));
       }
     })
-    .catch(err => {
-      if (err.name === "CastError") {
+    .catch((err) => {
+      if (err.name === 'CastError') {
         // Логика обработки ошибки
-        const err = new Error('Переданы некорректные данные');
-        err.statusCode = 400;
+        const errNew = new Error('Переданы некорректные данные');
+        errNew.statusCode = 400;
 
-        next(err);
+        next(errNew);
       }
       next(err);
     });
 };
 
 module.exports.likeCard = (req, res, next) => {
-
   Card.findByIdAndUpdate(
 
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-    { new: true }
+    { new: true },
   )
-    .then(card => {
+    .then((card) => {
       if (!card) {
         const err = new Error('Пользователь с указанным _id не найден');
         err.statusCode = 404;
 
         next(err);
       } else {
-        return res.send({ data: card })
+        res.send({ data: card });
       }
     })
-    .catch(err => {
-      if (err.name === "CastError") {
+    .catch((err) => {
+      if (err.name === 'CastError') {
         // Логика обработки ошибки
-        const err = new Error('Переданы некорректные данные для постановки лайка');
-        err.statusCode = 400;
+        const errNew = new Error('Переданы некорректные данные для постановки лайка');
+        errNew.statusCode = 400;
 
         next(err);
       }
       next(err);
-    })
+    });
 };
 
 module.exports.dislikeCard = (req, res, next) => {
@@ -95,26 +86,26 @@ module.exports.dislikeCard = (req, res, next) => {
 
     req.params.cardId,
     { $pull: { likes: req.user._id } }, // убрать _id из массива
-    { new: true }
+    { new: true },
   )
-    .then(card => {
+    .then((card) => {
       if (!card) {
         const err = new Error('Пользователь с указанным _id не найден');
         err.statusCode = 404;
 
         next(err);
       } else {
-        return res.send({ data: card })
+        res.send({ data: card });
       }
     })
-    .catch(err => {
-      if (err.name === "CastError") {
+    .catch((err) => {
+      if (err.name === 'CastError') {
         // Логика обработки ошибки
-        const err = new Error('Переданы некорректные данные для снятия лайка');
-        err.statusCode = 400;
+        const errNew = new Error('Переданы некорректные данные для снятия лайка');
+        errNew.statusCode = 400;
 
-        next(err);
+        next(errNew);
       }
       next(err);
-    })
-}
+    });
+};
--- models/user.js
+++ models/user.js
@@ -6,23 +6,23 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: 2,
     maxlength: 30,
-    default: 'Жак-Ив Кусто'
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    default: 'Исследователь'
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
-      validator: function (v) {
+      validator: function validation(v) {
         return /^(http|https):\/\/[^ "]+\.[^ "]+$/.test(v);
       },
-      message: () => `Ссылка неверна`
-    }
+      message: () => 'Ссылка неверна',
+    },
   },
   email: {
     type: String,
@@ -31,14 +31,13 @@ const userSchema = new mongoose.Schema({
     validate: {
       validator: (v) => isEmail(v),
       message: 'Неправильный формат почты',
-    }
+    },
   },
   password: {
     type: String,
     required: true,
     minlength: 8,
-    select: false
-  }
-})
-
+    select: false,
+  },
+});
 module.exports = mongoose.model('user', userSchema);
