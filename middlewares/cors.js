673b5cc16b1522b95fbec4a2c95158f57f9d7b21
Vladislav Milushkov <radshura@yandex.ru>
2021-10-04 14:57:11 +0300

    full

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', origin);
   }
 
   next();
