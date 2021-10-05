8e568d8e44fee601f6990155c4429b8e67b71faf
Vladislav Milushkov <radshura@yandex.ru>
2021-10-05 20:57:49 +0300

    full

--- app.js
+++ app.js
@@ -23,6 +23,16 @@ const allowedCors = [
 */
 
 const app = express();
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  next();
+});
 
 /*
 app.use((req, res, next) => {
