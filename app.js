e492246632438452a0760f8b2cf493a7067b6202
Vladislav Milushkov <radshura@yandex.ru>
2021-10-05 20:30:14 +0300

    full

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', origin);
   }
 
   next();
