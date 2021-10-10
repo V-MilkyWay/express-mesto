f9e088c69e9c76e018a2149d4953ce932ffce700
Vladislav Milushkov <radshura@yandex.ru>
2021-10-10 11:37:04 +0300

    full

--- app.js
+++ app.js
@@ -2,15 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const { celebrate, Joi, errors } = require('celebrate');
-const routerUser = require('./routes/users');
-const routerCards = require('./routes/cards');
-const auth = require('./middlewares/auth');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-// импортируем controllers
-const { createUser, login } = require('./controllers/users');
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
+
+const api = require('./routes/api');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
@@ -19,50 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(requestLogger);
-
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().default('Жак-Ив Кусто').min(2).max(30),
-    about: Joi.string().default('Исследователь').min(2).max(30),
-    avatar: Joi.string().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png').pattern(/^(http|https):\/\/[^ "]+\.[^ "]+$/),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), createUser);
-
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), login);
-
-app.use(auth);
-app.use('/', routerUser);
-app.use('/', routerCards);
-app.use('*', (req, res, next) => {
-  const err = new Error('Cтраница не найдена');
-  err.statusCode = 404;
-
-  next(err);
-});
-
-app.use(errorLogger);
-
-app.use(errors());
-
-app.use((err, req, res) => {
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      // проверяем статус и выставляем сообщение в зависимости от него
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
-});
+app.use('/api', api);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
